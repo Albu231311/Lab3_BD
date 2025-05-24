6b247a49dfcf12328204c9d4012b6866fd687f42
Camila Richter <ric23183@uvg.edu.gt>
2025-05-24 14:28:10 -0600

    crear un nuevo curso

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5">
+    <h1 class="mb-4">Crear Nuevo Curso</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="mb-0">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @include('cursos.form')
+</div>
+@endsection
